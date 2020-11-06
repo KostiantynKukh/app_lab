4a89a60fd42cddeea3685deb1942692408ed2cf6
KostiantynKukh <kkuh1993@gmail.com>
2020-11-06 19:56:38 +0200

    made support section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
+  // burger start
+
   $('.nav__toggler').click(function() {
     $('.nav__toggler, .nav__list').toggleClass('active');
     $('body').toggleClass('locked');
-  }) 
+  });
+
+//burger end
+
+//accordion start
+
+$('.accordion__item').click(function() {
+  if($(this).hasClass('active')){
+    $(this).removeClass('active')
+  } else {
+    $('.accordion__item').removeClass('active');
+    $(this).toggleClass('active');
+  }  
+})
+
+//accordion end 
+  
 })
